37bf324563050da04637fb7057c1428e89324869
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-03-31 08:58:52 -0300

    Select

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,14 @@ app.post("/courses", async (request: Request, response: Response) => {
   await knex.raw("INSERT INTO courses (name) VALUES (?)", [name]);
 
   response.status(201).json();
-})
+});
+
+app.get("/courses", async (request: Request, response: Response) => {
+  //const courses = await knex.raw("SELECT * FROM courses");
+
+  const courses = await knex("courses").select().orderBy("name");
+
+  response.json(courses);
+});
 
 app.listen(3333, () => console.log(`Server is running on port 3333`))
